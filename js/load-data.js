ee82f3bab979ff37952e77dc939456cf86e63548
Victoria Krassova <v.krassova@yandex.ru>
2020-10-20 10:53:36 +0300

    начинает задание в module6-task1

---
+++ js/load-data.js
@@ -0,0 +1,9 @@
+"use strict";
+
+( () => {
+
+  const TIMEOUT_IN_MS = 10000;
+  const DATA_URL = `https://21.javascript.pages.academy/keksobooking/data`;
+  const POST_URL = `https://21.javascript.pages.academy/keksobooking`;
+
+}) ();
