be0f7bd3c1a3b6513b4818afae36db470164251e
Victoria Krassova <v.krassova@yandex.ru>
2020-10-27 12:03:14 +0300

    добавляет код для загрузки данных

--- js/load-data.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-( () => {
-
-  const TIMEOUT_IN_MS = 10000;
-  const DATA_URL = `https://21.javascript.pages.academy/keksobooking/data`;
-  const POST_URL = `https://21.javascript.pages.academy/keksobooking`;
-
-}) ();
